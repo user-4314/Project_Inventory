795fe81a583ec477817a1cb78ae3bbe0cf1f811a
林佑宥 <92373659+user-4314@users.noreply.github.com>
2023-06-03 16:56:06 +0800

    Update with php request
    about register

--- app/src/main/java/com/example/mybmi/RegisterActivity.java
+++ app/src/main/java/com/example/mybmi/RegisterActivity.java
@@ -40,10 +40,50 @@ public class RegisterActivity extends Activity {
         sure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(RegisterActivity.this,MainActivity.class);
-                startActivity(intent);
+                Accountstr = Account.getText().toString();
+                Passwordstr = Password.getText().toString();
+                String url = "http://140.136.151.67/Login.php";
+
+                if (Accountstr.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "員工資料處不能為空白!!", Toast.LENGTH_LONG).show();
+                } else {
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            result = response.trim();
+                            result_textView.setText(result);//跳出success的字串在textView裡
+                            if (result.equals("Record added successfully")) {
+                                Intent intent = new Intent();
+                                intent.setClass(RegisterActivity.this, MainActivity.class);
+                                Toast.makeText(RegisterActivity.this, "註冊成功", Toast.LENGTH_LONG).show();
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "帳號密碼已存在", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(RegisterActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("LoginName", Accountstr);//和php的參數做連結
+                            params.put("LoginPassword", Passwordstr);
+                            return params;
+
+                        }
+                    };
+
+                    RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
+                    requestQueue.add(stringRequest);
+
+                }
             }
-        });
+
+            });
+
+
     }
 }
